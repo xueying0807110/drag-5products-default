5f29b3127ff4ffcbf9d9c9f3fc20ebe676059c87
xueying0807110 <xueying0807110@gmail.com>
2026-06-12 20:30:04 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,22 @@
+window.PRODUCTS = [
+  {
+    "name": "Abstract Marbling",
+    "image": "product-images/Abstract Marbling.png"
+  },
+  {
+    "name": "Annual Ring Marbling",
+    "image": "product-images/Annual Ring Marbling.png"
+  },
+  {
+    "name": "Black Gold Marbling",
+    "image": "product-images/Black Gold Marbling.png"
+  },
+  {
+    "name": "Blue Cracked Marbling",
+    "image": "product-images/Blue Cracked Marbling.png"
+  },
+  {
+    "name": "Blue Marbling",
+    "image": "product-images/Blue Marbling.png"
+  }
+];
